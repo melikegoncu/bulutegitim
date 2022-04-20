941789eb95b73dd094688213423cf55d42d07637
Başak Bakar <basakbakar312@gmail.com>
2022-04-20 19:33:55 +0300

    Webde açılmama sorunu halledildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
-
 import 'package:bulutegitim/screens/wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
-   WidgetsFlutterBinding.ensureInitialized();
-   await Firebase.initializeApp();
-   runApp( const Wrapper());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyA5DzQqLkkwt_NFEGN19Mje3k7SLdeZBR8",
+          appId: "1:192475888110:web:507de23fc29cf2098bf6ff",
+          messagingSenderId: "192475888110",
+          projectId: "bulutegitim-2f12a"));
+  runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: Wrapper()));
 }
